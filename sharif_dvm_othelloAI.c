ef4b7d2a1ddb1c1353eacf8c82027eac88ca17d8
mehrandvm <amir.daneshvarmoein@gmail.com>
2019-01-10 02:10:25 +0330

    input method finalized

--- sharif_dvm_othelloAI.c
+++ sharif_dvm_othelloAI.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-void showmap(char const *pargv, int *pMapArray);
+void createmap(char const *pargv, int *pMapArray);
+void printmap(char const *pargv, int *pMapArray);
 
 int main(int argc, char const *argv[])
 {
     char const *pargv = &argv[1][0]; //start from second argv
     int MapArray[8][8]; // our game map 2d-array
     int i,j; // counters;
-    int* pMapArray = &MapArray[0][0]; //sending 2d-array to showmap function as a pointer
-    showmap(pargv,pMapArray); // puts argvs in 2d-array and prints the array
+    //int* pMapArray = &MapArray[0][0]; //sending 2d-array to showmap function as a pointer
+    createmap(pargv,&MapArray[0][0]); // puts argvs in 2d-array
+    printmap(pargv,&MapArray[0][0]); //  prints the array
     return 0;
 }
 
-void showmap(char const *pargv, int *pMapArray)
+void createmap(char const *pargv, int *pMapArray)
 {
-    int i; 
+    int i,j; 
     // nested for to convert chars to ints and put in array
     for (i = 0; i < 8; ++i)
     {
@@ -26,12 +28,18 @@ void showmap(char const *pargv, int *pMapArray)
         ++pargv;
         ++pargv;
     }
+}
+
+void printmap(char const *pargv, int *pMapArray)
+{
+    int i,j;
     // nested for to print the array
     for(j = 0; j < 8; j++)
     {
         for(i = 0; i < 8; i++)
         {
-            printf("%d ",MapArray[i][j]);
+            printf("%d ",*pMapArray);
+            *pMapArray++;
         }
         printf("\n");
     }
