0cf16d9f5b1558315efc3394703c603059460994
mehrandvm <amir.daneshvarmoein@gmail.com>
2019-01-10 01:18:16 +0330

    finalized input method and convert chat to int

--- sharif_dvm_othelloAI.c
+++ sharif_dvm_othelloAI.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
 
-void showmap(char const *p)
-{
-    int i;
-    for (i = 1; i <= 8; ++i)
-    {
-        for (; *p != '\0'; ++p)
-        {
-            printf("%c ", *p);
-        }
-        printf("\n");
-        ++p;
-        ++p;
-    }
-}
+void showmap(char const *pargv, int *pMapArray);
+
 int main(int argc, char const *argv[])
 {
-    char const *p = &argv[1][0];
-    showmap(p);
+    char const *pargv = &argv[1][0]; //start from second argv
+    int MapArray[8][8]; // our game map 2d-array
+    int i,j; // counters;
+    int* pMapArray = &MapArray[0][0]; //sending 2d-array to showmap function as a pointer
+    showmap(pargv,pMapArray); // puts argvs in 2d-array and prints the array
     return 0;
+}
+
+void showmap(char const *pargv, int *pMapArray)
+{
+    int i; 
+    // nested for to convert chars to ints and put in array
+    for (i = 0; i < 8; ++i)
+    {
+        for (; *pargv != '\0'; pargv++)
+        {
+            *pMapArray = *pargv-48;
+            pMapArray++;
+        }
+        ++pargv;
+        ++pargv;
+    }
+    // nested for to print the array
+    for(j = 0; j < 8; j++)
+    {
+        for(i = 0; i < 8; i++)
+        {
+            printf("%d ",MapArray[i][j]);
+        }
+        printf("\n");
+    }
 }
