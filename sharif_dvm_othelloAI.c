29af680e21291431602cb8893dbba8ab030cb519
mehrandvm <amir.daneshvarmoein@gmail.com>
2019-01-09 17:01:23 +0330

    dvm:get argv input and print map done

--- sharif_dvm_othelloAI.c
+++ sharif_dvm_othelloAI.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main(){
-
-return 0;
+void showmap(char const *p)
+{
+    int i;
+    for (i = 1; i <= 8; ++i)
+    {
+        for (; *p != '\0'; ++p)
+        {
+            printf("%c ", *p);
+        }
+        printf("\n");
+        ++p;
+        ++p;
+    }
+}
+int main(int argc, char const *argv[])
+{
+    char const *p = &argv[1][0];
+    showmap(p);
+    return 0;
 }
